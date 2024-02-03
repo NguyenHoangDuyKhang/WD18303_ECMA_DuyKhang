e4661c7eab493c31729ede2b0e4986db98d46de2
NguyenHoangDuyKhang <khang12a3lqd@gmail.com>
2024-02-03 19:29:48 +0700

    mhahahdjashdksah

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const a  = 10;
+          const a  = 10;
 
-// console.log(a);
+        console.log(a);
 // Chỗ này sẽ báo lỗi vì khai báo biến a mà không dùng!!!
 
-const getname = (name) => {
-  // return `hello ${name}`;
-};
+          const getname = (name) => {
+             return `hello ${name}`;
+              };
 
 // console.log(getname('Khang'));
